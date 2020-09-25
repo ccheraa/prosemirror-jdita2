0349e49e371844124e2ac4111d891787a074cbc5
Charafeddine Cheraa <ccheraa@gmail.com>
2020-09-25 12:01:53 +0100

    a function to create nodes with default content

--- src/commands.ts
+++ src/commands.ts
@@ -4,10 +4,19 @@ import { Command } from 'prosemirror-commands';
 import { Fragment, Node, Schema } from 'prosemirror-model';
 import { TextSelection } from 'prosemirror-state';
 
+export function createNode(schema: Schema, type: string): Node {
+  switch (type) {
+    case 'p': return schema.nodes[type].createAndFill() as Node;
+    case 'li': return schema.nodes[type].createAndFill({}, createNode(schema, 'p')) as Node;
+  }
+  throw new Error('unkown node type');
+}
+
 export function newLine(schema: Schema): Command {
-  const splittable: Record<string, number> = {
+  const allowedNodes: Record<string, number> = {
     p: 1,
     li: 2,
+    dd: 2,
   };
   return function (state, dispatch) {
     let { $from, $to } = state.selection;
@@ -18,7 +27,7 @@ export function newLine(schema: Schema): Command {
     let deleteParent = '';
     for (let i = $from.depth; i > 0; i--) {
       parent = $from.node(i);
-      if (splittable[parent.type.name]) {
+      if (allowedNodes[parent.type.name]) {
         grandParent = $from.node(i - 1);
         depth = $from.depth - i + 1;
         if (parent.content.size + deletionDepth * 2 == 0 && grandParent.childCount == $from.indexAfter(deletionDepth - 1)) {
@@ -26,53 +35,40 @@ export function newLine(schema: Schema): Command {
           deleteParent = parent.type.name;
           continue;
         }
-        console.log('depth', depth);
         break;
       }
     }
     if (!parent || !grandParent) {
       return false;
     }
-    console.group('splitting:', parent.type.name);481
-    console.log('parent:', parent);
-    console.log('$from:', $from);
-    console.log('$to:', $to);
-    switch (parent.type.name) {
-      case 'p':
-      case 'li':
-        let tr = state.tr.delete($from.pos, $to.pos);
-        if (!canSplit(tr.doc, $from.pos - (deleteParent ? 1 : 0))) {
-          console.log('can\'t split');
-          console.groupEnd();
-          return false;
-        }
-        if (deleteParent && depth === splittable[deleteParent]) {
-          console.log('can\'t split further');
-          console.groupEnd();
-          return false;
-        }
-        if (dispatch) {
-          const EOL = $to.end() === $to.pos;
-          if (!deleteParent && EOL) {
-            console.log('end of line');
-            let content: Fragment | undefined;
-            if (parent.type.name === 'li') {
-              content = Fragment.from(schema.nodes.p.createAndFill() as Node);
-            }
-            tr = tr.insert($to.pos + 1, Fragment.from(parent.type.createAndFill({}, content) as Node));
-            const pos = tr.selection.$to.doc.resolve(tr.selection.$to.pos + 2 * depth);
-            const newSelection = new TextSelection(pos, pos);
-            dispatch(tr.setSelection(newSelection).scrollIntoView());
-          } else {
-            dispatch(tr.split($from.pos - (deleteParent ? 1 : 0), depth - (deleteParent ? 1 : 0)).scrollIntoView());
-          }
-        }
-        console.groupEnd();
-        return true
-      default:
-        console.log('not implemented yet');
+    console.group('splitting:', parent.type.name); 481
+    if (allowedNodes[parent.type.name]) {
+      let tr = state.tr.delete($from.pos, $to.pos);
+      if (!canSplit(tr.doc, $from.pos - (deleteParent ? 1 : 0))) {
         console.groupEnd();
         return false;
+      }
+      if (deleteParent && depth === allowedNodes[deleteParent]) {
+        console.groupEnd();
+        return false;
+      }
+      if (dispatch) {
+        const EOL = $to.end() === $to.pos;
+        if (!deleteParent && EOL) {
+          let content: Fragment | undefined;
+          tr = tr.insert($to.pos + 1, createNode(schema, parent.type.name));
+          const pos = tr.selection.$to.doc.resolve(tr.selection.$to.pos + 2 * depth);
+          const newSelection = new TextSelection(pos, pos);
+          dispatch(tr.setSelection(newSelection).scrollIntoView());
+        } else {
+          dispatch(tr.split($from.pos - (deleteParent ? 1 : 0), depth - (deleteParent ? 1 : 0)).scrollIntoView());
+        }
+      }
+      console.groupEnd();
+      return true;
+    } else {
+      console.groupEnd();
+      return false;
     }
   }
 }
